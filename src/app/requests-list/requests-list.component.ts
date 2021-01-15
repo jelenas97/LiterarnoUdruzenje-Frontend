c842d4fc8ea1b49cbf2461b836a93c24b79eeba8
syn-petarcurcin <petar.curcin1@synechron.com>
2021-01-15 23:33:48 +0100

    requests list page

---
+++ src/app/requests-list/requests-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {CamundaService} from '../services/camunda/camunda.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-requests-list',
+  templateUrl: './requests-list.component.html',
+  styleUrls: ['./requests-list.component.css']
+})
+export class RequestsListComponent implements OnInit {
+  tasks: any;
+
+  constructor(private camundaService: CamundaService, private router: Router) { }
+
+  ngOnInit(): void {
+    const x = this.camundaService.getUserTasks("boardMember1");
+
+    x.subscribe(
+      res => {
+        console.log(res);
+        this.tasks = res;
+      },
+      err => {
+        console.log('Error occured');
+      }
+    );
+  }
+
+  review(taskId: any) {
+    this.router.navigate(['/registrate/' + taskId]);
+
+  }
+}
